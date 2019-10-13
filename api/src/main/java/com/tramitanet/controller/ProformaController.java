8f128aea3c1f12daea5e009c0d7b0c2c9d00244b
Hugo Armando Martinez <h.m.chamonica@evomatik.com>
2019-10-13 16:11:03 -0500

    Se agrega crossorigin headers para solucionar incidente

--- api/src/main/java/com/tramitanet/controller/ProformaController.java
+++ api/src/main/java/com/tramitanet/controller/ProformaController.java
@@ -10,6 +10,7 @@ import javax.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.util.ObjectUtils;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -25,6 +26,7 @@ import com.tramitanet.model.Proforma;
  * @author evomatik
  *
  */
+@CrossOrigin(origins = "http://localhost:4200")
 @RestController
 @RequestMapping("/tramitanet")
 public class ProformaController {
