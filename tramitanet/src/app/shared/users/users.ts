ecb61d7b8bf90896408d92d63b992264b30abef1
Hugo Armando Martinez <h.m.chamonica@evomatik.com>
2019-10-19 21:26:42 -0500

    Se agregan cambios para descarga archivo capturista

--- tramitanet/src/app/shared/users/users.ts
+++ tramitanet/src/app/shared/users/users.ts
@@ -10,7 +10,9 @@ export class Users {
         { username: 'admin', email: 'admin@company.com', claveCliente: 'CLI0001', razonSocial: 'FEDRYL SA DE CV', password: 'admin', role: 4 },
         { username: 'cliente', email: 'gm_motors@gmail.com', claveCliente: 'CLI0001', password: '   ', role: 1 },
         { username: 'analista', email: 'ernesto_trejo@hotmail.com', password: '4n4l1st4', role: 2 },
-        { username: 'capturista', email: 'j.m.r@hotmail.com', password: 'c4ptur1st4', role: 3 },
+        { username: 'capturista', email: 'j.m.r@hotmail.com', claveCapturista: 'BONI', password: 'c4ptur1st4', role: 3 },
+        { username: 'capturista2', email: 'capturista2@gmail.com', claveCapturista: 'JAVIER', password: 'capturista2', role: 3 },
+        
         // se agregan usuarios de prueba
         { username: 'cliente', email: 'cliente', claveCliente: 'CLI0002', password: 'cliente', role: 1 },
         { username: 'analista', email: 'analista', password: 'analista', role: 2 },
