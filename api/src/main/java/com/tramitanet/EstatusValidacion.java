1f911acc1acbbdc4eedb92ce67b6442dd9d0b51c
Hugo Armando Martinez <h.m.chamonica@evomatik.com>
2019-11-03 22:55:00 -0600

    cambios para conexion a base de datos

--- api/src/main/java/com/tramitanet/EstatusValidacion.java
+++ api/src/main/java/com/tramitanet/EstatusValidacion.java
@@ -8,5 +8,27 @@ package com.tramitanet;
  *
  */
 public enum EstatusValidacion {
+	APROBADO("Aprobado"),ERROR("Error");
+	
+	EstatusValidacion(String estatus){
+		this.estatus=estatus;
+	}
+	
+	private String estatus;
 
+	/**
+	 * @return the estatus
+	 */
+	public String getEstatus() {
+		return estatus;
+	}
+
+	/**
+	 * @param estatus the estatus to set
+	 */
+	public void setEstatus(String estatus) {
+		this.estatus = estatus;
+	}
+	
+			
 }
